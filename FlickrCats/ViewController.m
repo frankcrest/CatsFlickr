9f16d59236fd6f1aeaf695f40a623e1758e28c46
Frank Chen <frankcrestview@hotmail.com>
2019-05-16 16:39:45 -0400

    Make the GET request using NSURLSession

--- FlickrCats/ViewController.m
+++ FlickrCats/ViewController.m
@@ -10,13 +10,58 @@
 
 @interface ViewController ()
 
+@property
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self networkToFlickr];
+     }
+
+-(void)networkToFlickr{
+    NSURL *url = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=ee7022884e6a1d08111f705e8e1afba8&tags=cat"]; // 1
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) { // 1
+            // Handle the error
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSError *jsonError = nil;
+        NSDictionary *flickrDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError]; // 2
+        
+        if (jsonError) { // 3
+            // Handle the error
+            NSLog(@"jsonError: %@", jsonError.localizedDescription);
+            return;
+        }
+    
+        NSDictionary* photosDictionary = [flickrDictionary objectForKey:@"photos"];
+        NSArray* photoDictionary = [photosDictionary objectForKey:@"photo"];
+        for (NSDictionary* dictionary in photoDictionary) {
+            <#statements#>
+        }
+        
+        
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+           
+        }];
+    }]; // 5
+    
+    [dataTask resume]; // 6
+    
+    
 }
 
 
